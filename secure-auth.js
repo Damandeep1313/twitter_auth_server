8547bb7a9911ae7a96622b75644823f60456e3b7
Damandeep Singh <99653389+Damandeep1313@users.noreply.github.com>
2025-04-06 00:00:09 +0530

    Update secure-auth.js

--- secure-auth.js
+++ secure-auth.js
@@ -71,7 +71,7 @@ app.get(`${base}/callback`, async (req, res) => {
       return res.send(`
         <h1>✅ Success!</h1>
         <p>YOUR AUTHORIZATION TOKEN:</p>
-        <p><code>${bearerToken}</code></p>
+        <h1><code>${bearerToken}</code></h1>
         <p>Store it securely. This gives access to your Twitter account.</p>
         `);
     } else {
